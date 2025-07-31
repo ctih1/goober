33aad8b144a502721381b041c226aebaaee0c35f
ctih1 <nevalaonni@gmail.com>
2025-07-31 23:18:26 +0300

    Updated songchanger

--- assets/cogs/songchanger.py
+++ assets/cogs/songchanger.py
@@ -2,8 +2,10 @@ import discord
 from discord.ext import commands
 from modules.globalvars import RED, GREEN, RESET, LOCAL_VERSION_FILE
 import os
-
+from modules.settings import ActivityType, instance as settings_manager
 from modules.permission import requires_admin
+
+from typing import get_args, Dict
 
 
 class songchange(commands.Cog):
@@ -33,6 +35,40 @@ class songchange(commands.Cog):
         except Exception as e:
             print(f"{RED}An error occurred while changing songs..: {str(e)}{RESET}")
 
+    @requires_admin()
+    @commands.command()
+    async def change_activity(self, ctx, type: str | None, *string):
+        if type not in get_args(ActivityType):
+            await ctx.send(f"Type needs to be one of the following: {', '.join(get_args(ActivityType))}")
+            return 
+        
+        settings_manager.settings["bot"]["misc"]["activity"] = { # type: ignore
+            "type": type,
+            "content": ' '.join(string)
+        }
+
+        settings_manager.commit()
+
+            
+        activities: Dict[ActivityType, discord.ActivityType] = {
+            "listening": discord.ActivityType.listening,
+            "playing": discord.ActivityType.playing,
+            "streaming": discord.ActivityType.streaming,
+            "competing": discord.ActivityType.competing,
+            "watching": discord.ActivityType.watching,
+        }
+
+        await self.bot.change_presence(
+            activity=discord.Activity(
+                type=activities.get(
+                    settings_manager.settings["bot"]["misc"]["activity"]["type"], # type: ignore
+                    discord.ActivityType.unknown,
+                ),
+                name=settings_manager.settings["bot"]["misc"]["activity"]["content"],
+            )
+        )
+
+        await ctx.send("Changed activity!")
 
 async def setup(bot):
     await bot.add_cog(songchange(bot))
