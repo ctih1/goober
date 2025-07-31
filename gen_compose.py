9524e7e5b80ecb3870a164380a4bc1481681a48d
ctih1 <nevalaonni@gmail.com>
2025-07-31 23:44:03 +0300

    updated compose command to save to home

--- gen_compose.py
+++ gen_compose.py
@@ -1,3 +1,7 @@
+import os
+
+home = os.path.expanduser("~")
+
 with open("docker-compose.yml", "w") as f:
     f.write("version: '3'\nservices:\n")
     for i in range(1, 31):
@@ -8,7 +12,7 @@ with open("docker-compose.yml", "w") as f:
       - DISCORD_BOT_TOKEN=${{TOKEN_{i}}}
       - BOT_INDEX={i}
     volumes:
-      - ./data/bot{i}:/app/data
-      - ./data/bot{i}/settings:/app/settings
+      - {home}/data/bot{i}:/app/data
+      - {home}/data/bot{i}/settings:/app/settings
     restart: unless-stopped
 """)
