51614ec2f68034c46f4e92faa8310973eb284828
ctih1 <78687256+ctih1@users.noreply.github.com>
2026-04-30 07:53:23 +0300

    Updated translator

--- assets/cogs/song_translator.py
+++ assets/cogs/song_translator.py
@@ -34,10 +34,9 @@ class LRCLIBResponse(TypedDict):
 
 
 class WaitingObject(TypedDict):
-    video_id: str
+    video_url: str
     options: List[LRCLIBResponse]
     message: discord.Message
-    video_length: float
 
 
 class SongTranslator(commands.Cog): 
@@ -114,6 +113,7 @@ class SongTranslator(commands.Cog):
         return subtitles
 
     async def translate_and_combine(self, message: discord.Message, video_id: str, video_length: float, lyrics: str, synced: bool, offset: float = 0.0) -> None:
+        logger.info(f"Translating lyrics synced? {synced} ({lyrics[4:]})")
         await message.edit(content="Translating lyrics...")
 
         if not synced:
@@ -127,13 +127,14 @@ class SongTranslator(commands.Cog):
         path = os.path.abspath(f"data/youtube/{video_id}").replace("\\", "/")
         logger.info(path)
 
-        await message.edit(content="Burning lyrics onto video")
+        await message.edit(content="Burning lyrics onto video...")
 
         command = f'{os.environ.get("FFMPEG_PATH", "ffmpeg")} -i {path}.mp4 {os.environ.get("FFMPEG_ARGS", "")} -vf "subtitles=filename={path}.srt" {path}_sub.mp4'
         logger.info(command)
         code = os.system(command)
         if code != 0:
             logger.error(f"ffmpeg exited with {code}")
+            await message.edit(content=f"FF Mpreg failed with error {code}")
             return
     
         logger.info("Done")
@@ -150,30 +151,33 @@ class SongTranslator(commands.Cog):
     def get_first_lyric_time(self, lyrics: str) -> str:
         return lyrics.split("\n")[0].split("]", 1)[0].replace("[", "")
 
-    @commands.command()
-    async def translate(self, ctx: commands.Context, *args):
-        logger.info(f"Trying {' '.join(args[1:])}")
-        url = args[0]
-
-        message = await send_message(ctx, "Searching for video...")
-
-        if not url.startswith("https://www.youtube.com"):
-            await send_message(ctx, "Please give a YouTube link!! (starting with https://www.youtube.com)")
-            return
+    async def download_video(self, url: str, user_id: int, message: discord.Message) ->  AsyncYouTube:
+        await message.edit(content="Fetching video data...")
 
         video = AsyncYouTube(url)
 
-        if await video.length() > 5*60 and not is_admin(ctx.author.id):
+        if await video.length() > 5*60 and not is_admin(user_id):
             await message.edit(content="Video is too long!! Consult an admin to download xwx")
         
         await message.edit(content=f"Found video '{await video.title()}', downloading")
         stream = (await video.streams()).filter(progressive=True, file_extension="mp4")
         target_stream = stream.get_by_resolution("480p") or stream.get_by_resolution("360p") or stream.get_by_resolution("240p") or stream.get_by_resolution("144p")
 
-        start_time = time.time()
         target_stream.download(f"data/youtube", filename=video.video_id+".mp4") # type: ignore
 
-        await message.edit(content=f"Downloaded clip in {round(time.time()-start_time, 3)}s! Fetching lyrics")
+        return video
+
+
+    @commands.command()
+    async def translate(self, ctx: commands.Context, *args):
+        logger.info(f"Trying {' '.join(args[1:])}")
+        url: str = args[0]
+
+        if not url.startswith(("https://www.youtube.com", "https://youtu.be")):
+            await send_message(ctx, "Please give a YouTube link!! (starting with https://www.youtube.com)")
+            return
+
+        message = await send_message(ctx, "Searching for subtitles...")
 
         response = requests.get(f"https://lrclib.net/api/search?q={slugify(' '.join(args[1:]), separator='+')}")
         matches: List[LRCLIBResponse] = response.json()
@@ -194,41 +198,45 @@ class SongTranslator(commands.Cog):
 
             self.waiting_ids[ctx.author.id] = {
                 "options": matches,
-                "video_id": video.video_id,
-                "video_length": await video.length(),
+                "video_url": url,
                 "message": message
             }
+
+            return
         
         else:
             synced_lyrics = matches[0]["syncedLyrics"]
             lyrics = matches[0]["plainLyrics"]
 
-            await self.translate_and_combine(message, video.video_id, await video.length(), synced_lyrics or lyrics, bool(synced_lyrics))
+        video = await self.download_video(url, ctx.author.id, message)
+
+        await self.translate_and_combine(message, video.video_id, await video.length(), synced_lyrics or lyrics, bool(synced_lyrics))
     
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
         waiting_object: WaitingObject | None = self.waiting_ids.get(message.author.id)
 
-        if not waiting_object:
+        if self.bot.user not in message.mentions or not waiting_object:
             return
 
-        if self.bot.user in message.mentions and waiting_object:
-            try:
-                parts = message.content.split(" ")
-                offset = 0
+        try:
+            parts = message.content.split(" ")
+            offset = 0
 
-                if len(parts) > 1:
-                    offset = float(parts[1])
+            if len(parts) > 1:
+                offset = float(parts[1])
 
-                song_id = int(parts[0])-1
+            song_id = int(parts[0])-1
 
-                song = waiting_object["options"][song_id]
-                synced_lyrics = song["syncedLyrics"]
-                lyrics = song["plainLyrics"]
+            song = waiting_object["options"][song_id]
+            synced_lyrics = song["syncedLyrics"]
+            lyrics = song["plainLyrics"]
 
-                await self.translate_and_combine(waiting_object["message"], waiting_object["video_id"], waiting_object["video_length"], synced_lyrics or lyrics, bool(synced_lyrics), offset)
-            except ValueError:
-                logger.info("Failed to convert")
+            video = await self.download_video(waiting_object["video_url"], message.author.id, waiting_object["message"])
+
+            await self.translate_and_combine(waiting_object["message"], video.video_id, await video.length(), synced_lyrics or lyrics, bool(synced_lyrics), offset)
+        except ValueError:
+            logger.info("Failed to convert")
                 
 
 async def setup(bot):
