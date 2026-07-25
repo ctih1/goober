01d77e7429ce5759ab04fb17b654f25b0b3129aa
ctih1 <onni09nevala@gmail.com>
2026-07-25 16:30:14 +0300

    updated IP

--- assets/cogs/ai_larper.py
+++ assets/cogs/ai_larper.py
@@ -8,27 +8,31 @@ import os
 import json
 import random
 
+
 class AILarper(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.description = "🧠|Interact with the goober gang models (2)"
+        self.temp = 0.9
 
         with open("assets/fonts/attachments.json", "r") as f:
             self.cache = json.load(f)
-
 
     @commands.command()
     async def larp(self, ctx: Context, *, content: str):
         content = content.strip()
 
         async with ctx.typing():
-            response: Response = await requests_async.post("http://localhost:6655/generate", json={"prompt": content}, headers={
-                "X-Auth": os.environ.get("AI_KEY")
-            })
+            response: Response = await requests_async.post(
+                "http://192.168.100.25:6655/generate",
+                json={"prompt": content, "temperature": self.temp},
+                headers={"X-Auth": os.environ.get("AI_KEY")},
+            )
         if response.status_code != 200:
-            await send_message(ctx, f"Failed to generateo: {response.status_code} {response.text}")
+            await send_message(
+                ctx, f"Failed to generateo: {response.status_code} {response.text}"
+            )
             return
-        
 
         text = response.text
         text = text.replace("[attachment]", random.choice(self.cache)["url"], 1)
@@ -38,16 +42,28 @@ class AILarper(commands.Cog):
 
         await send_message(ctx, text)
 
+    @commands.command()
+    async def set_temp(self, ctx: Context, temperature: str) -> None:
+        self.temp = float(temperature)
+
+        await send_message(ctx, "Set temp")
 
     @commands.command()
     async def check_larp(self, ctx: Context):
-        response: Response = await requests_async.get("http://localhost:6655/history")
-        await send_message(ctx, f"```json\n{json.dumps(response.json(), indent=4)}\n```")
+        response: Response = await requests_async.get(
+            "http://192.168.100.25:6655/history"
+        )
+        await send_message(
+            ctx, f"```json\n{json.dumps(response.json(), indent=4)}\n```"
+        )
 
     @commands.command()
     async def clear_larp(self, ctx: Context):
-        response: Response = await requests_async.post("http://localhost:6655/clear")
+        response: Response = await requests_async.post(
+            "http://192.168.100.25:6655/clear"
+        )
         await send_message(ctx, f"Cleared larp")
+
 
 async def setup(bot):
     await bot.add_cog(AILarper(bot))
